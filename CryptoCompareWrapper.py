00914c3ca2f046bf59b71f24ad79a07c39336888
Avi Felman <avifelman@Avis-MacBook-Pro.local>
2018-11-25 19:17:52 -0500

    first commit

--- CryptoCompareWrapper.py
+++ CryptoCompareWrapper.py
@@ -1,4 +1,8 @@
 ## In this file I will be create a wrapper for the CryptoCompare API, with the goal to be able to pull any coins last 30 day history
 ## and get a list of the top 50 coins at any given moment.
 
+##  top 30 coins at any given time, along with their marketcap at certain date
+
+
+## Construct initial portfolio  
 
